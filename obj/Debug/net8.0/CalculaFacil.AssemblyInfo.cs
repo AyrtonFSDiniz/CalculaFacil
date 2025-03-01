a5b20e78607227909cb3877daf58de12b48c6447
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2025-03-01 07:16:19 -0300

    [feat] - Adicionando arquivo ads.txt.

--- obj/Debug/net8.0/CalculaFacil.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculaFacil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculaFacil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be9f1dba2ac3be2cb926dc27074f3ea65da3f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141269069f96d3de261c6f0a6a1e9bfa671edcfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculaFacil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculaFacil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
